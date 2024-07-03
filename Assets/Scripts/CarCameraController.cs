d146f31a69942ff1cedf6baef718de36c2b36eb8
Mahamat335 <gcanbolat46@gmail.com>
2024-07-03 23:47:25 +0300

    sa

--- Assets/Scripts/CarCameraController.cs
+++ Assets/Scripts/CarCameraController.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
-public class CarCameraController : MonoBehaviour {
+public class CarCameraController : MonoBehaviour
+{
     public float mouseSensitivity = 100f;
 
     public float xClamp = 80;
@@ -10,20 +11,23 @@ public class CarCameraController : MonoBehaviour {
     private float yRotation = 0f;
     public float crouchingSpeed = 3f;
     private Vector3 defaultPosition = new Vector3(-1.0f, 1.0f, 0.0f);
-    private Vector3 crouchedPosition = new Vector3(-0.2f, 0.2f, 0.0f);
+    private Vector3 crouchedPosition = new Vector3(-0.3f, 0.3f, 0.0f);
 
 
-    void Start() {
+    void Start()
+    {
         Cursor.lockState = CursorLockMode.Locked;
         transform.rotation = Quaternion.Euler(0, 90, 0f);
     }
 
-    void LateUpdate() {
+    void LateUpdate()
+    {
         LookAround();
         Move();
     }
 
-    private void LookAround() {
+    private void LookAround()
+    {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
@@ -35,37 +39,43 @@ public class CarCameraController : MonoBehaviour {
 
         transform.localRotation = Quaternion.Euler(xRotation, yRotation + 90, 0f);
 
-        if (transform.localRotation.eulerAngles.x > 55.0f) {
+        if (transform.localRotation.eulerAngles.x > 45.0f)
+        {
             transform.localPosition = Vector3.Lerp(transform.localPosition, defaultPosition - crouchedPosition, Time.deltaTime * crouchingSpeed);
         }
-        else {
+        else
+        {
             transform.localPosition = Vector3.Lerp(transform.localPosition, defaultPosition, Time.deltaTime * crouchingSpeed);
         }
     }
 
-    private void Move() {
+    private void Move()
+    {
 
 
 
-        if (Input.GetKey(KeyCode.LeftArrow)) {
-
-            yClamp = 18;
-            xClamp = 55;
-                defaultPosition = new Vector3(-1.0f, 1.0f, 0.3f);
-
-        }
-        else if (Input.GetKey(KeyCode.RightArrow)) {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
 
             yClamp = 18;
             xClamp = 55;
-                defaultPosition = new Vector3(-1.0f, 1.0f, -0.3f);
+            defaultPosition = new Vector3(-1.0f, 1.0f, 0.3f);
+
         }
-        else {
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+
+            yClamp = 18;
+            xClamp = 55;
+            defaultPosition = new Vector3(-1.0f, 1.0f, -0.3f);
+        }
+        else
+        {
 
             yClamp = 30;
             xClamp = 80;
-                defaultPosition = new Vector3(-1.0f, 1.0f, 0.0f);
-            }
+            defaultPosition = new Vector3(-1.0f, 1.0f, 0.0f);
+        }
 
         transform.localPosition = Vector3.Lerp(transform.localPosition, defaultPosition, Time.deltaTime * crouchingSpeed);
 
--- Assets/Scripts/ManualWindowControl.cs
+++ Assets/Scripts/ManualWindowControl.cs
@@ -2,7 +2,8 @@
 using UnityEngine;
 using System.Collections;
 
-public class ManualWindowControl : MonoBehaviour {
+public class ManualWindowControl : MonoBehaviour
+{
 
     public GameObject MWRotaterR;
     public GameObject glass_backR;
@@ -10,31 +11,36 @@ public class ManualWindowControl : MonoBehaviour {
     public GameObject glass_backL;
     public Camera mainCamera;
 
-    public float rotationSpeed = 360f;
+    public float rotationSpeed = 6f;
     public float movementSpeed = 0.058f;
 
     private bool isRotatingR = false;
     private bool isRotatingL = false;
 
-    void Update() {
+    void FixedUpdate()
+    {
 
         CheckAndControlWindow();
 
     }
 
-    private void CheckAndControlWindow() {
+    private void CheckAndControlWindow()
+    {
 
         float camYRot = mainCamera.transform.eulerAngles.y;
         float camXRot = mainCamera.transform.eulerAngles.x;
 
-        if (camXRot >= 30 && camXRot < 55) {
+        if (camXRot >= 30 && camXRot < 55)
+        {
 
-            if (camYRot >= 115 && camYRot <= 125) {
+            if (camYRot >= 115 && camYRot <= 125)
+            {
 
                 ControlWindow(MWRotaterR, glass_backR, ref isRotatingR);
 
             }
-            else if (camYRot >= 55 && camYRot <= 65) {
+            else if (camYRot >= 55 && camYRot <= 65)
+            {
 
                 ControlWindow(MWRotaterL, glass_backL, ref isRotatingL);
 
@@ -44,9 +50,11 @@ public class ManualWindowControl : MonoBehaviour {
 
     }
 
-    private void ControlWindow(GameObject rotater, GameObject glass, ref bool isRotating) {
+    private void ControlWindow(GameObject rotater, GameObject glass, ref bool isRotating)
+    {
 
-        if (Input.GetKey(KeyCode.W) && !isRotating && glass.transform.localPosition.y > 0.71f) {
+        if (Input.GetKey(KeyCode.W) && !isRotating && glass.transform.localPosition.y > 0.71f)
+        {
 
             StartCoroutine(
 
@@ -55,7 +63,8 @@ public class ManualWindowControl : MonoBehaviour {
             );
 
         }
-        else if (Input.GetKey(KeyCode.S) && !isRotating && glass.transform.localPosition.y < 1.0f) {
+        else if (Input.GetKey(KeyCode.S) && !isRotating && glass.transform.localPosition.y < 1.0f)
+        {
 
             StartCoroutine(RotateAndMove(rotater, glass, rotationSpeed, movementSpeed, isRotating));
 
@@ -71,34 +80,31 @@ public class ManualWindowControl : MonoBehaviour {
         float moveSpeed,
         bool isRotating
 
-    ) {
+    )
+    {
         isRotating = true;
 
-        while ((Input.GetKey(KeyCode.W) && rotSpeed < 0) || (Input.GetKey(KeyCode.S) && rotSpeed > 0)) {
+        while ((Input.GetKey(KeyCode.W) && rotSpeed < 0) || (Input.GetKey(KeyCode.S) && rotSpeed > 0))
+        {
+            rotater.transform.localEulerAngles += new Vector3(0, 0, rotSpeed * Time.fixedDeltaTime);
 
-            float newRotZ = rotater.transform.localEulerAngles.z + rotSpeed * Time.deltaTime;
-
-            if (newRotZ < 0)
-                newRotZ += 360;
-            if (newRotZ > 360)
-                newRotZ -= 360;
-
-            rotater.transform.localEulerAngles = new Vector3(0, 0, newRotZ);
-
-            float newY = glass.transform.localPosition.y + moveSpeed * Time.deltaTime;
-            newY = Mathf.Clamp(newY, 0.71f, 1.0f);
+            float newY = glass.transform.localPosition.y + moveSpeed * Time.fixedDeltaTime;
+            if (newY < 0.71f)
+                break;
+            if (newY > 1)
+                break;
             glass.transform.localPosition = new Vector3(
                 glass.transform.localPosition.x,
                 newY,
                 glass.transform.localPosition.z
             );
 
-            yield return null;
-
+            yield return new WaitForFixedUpdate();
         }
 
         isRotating = false;
 
     }
-
 }
+
+
